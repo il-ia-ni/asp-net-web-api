2fdf28ee03a5a01b9ed22bd1f438c32c7d7341cd
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-07 19:08:17 +0200

    Initial commit

---
+++ TrackingAPI/Tracking-api/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace Tracking_api
+{
+    public class WeatherForecast
+        /* A model to display a web api with the weather forecast */
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
