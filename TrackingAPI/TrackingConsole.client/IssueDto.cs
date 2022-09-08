1a87ef6c0727102678ca98ead17edd33eb210202
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-08 10:16:42 +0200

    Added logic for reading data over the web API to the console client

---
+++ TrackingAPI/TrackingConsole.client/IssueDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingConsole.client
+{
+    internal class IssueDto
+        // Data Transfer Object is similar to Models in APIs
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
--- TrackingAPI/TrackingConsole.client/Program.cs
+++ TrackingAPI/TrackingConsole.client/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using TrackingConsole.client;
+
+HttpClient client = new();  // HttpClient cls is used in .Net to make HTTP requests and comes with built-in methods for this
+
+client.BaseAddress = new Uri("https://localhost:7070");  // sets as a base URL for HTTP reqs the url of the Web API project(located in Properties/launchSettings.json -> Profiles/Tracking_api)
+
+// tells the server the result of reqs must be in JSON format
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage response = await client.GetAsync("api/issue");  // a url of the endpoint is required to make the async get req
+response.EnsureSuccessStatusCode();  // sets IsSuccessStatusCode to true if the response was successful
+
+if (response.IsSuccessStatusCode)
+{
+    var issues = await response.Content.ReadFromJsonAsync <IEnumerable <IssueDto>> ();  // Content prop's method ReadFromJsonAsync deserializes the content of the response into IEnumerable of IssueDto (Data Transfer Object == Model of API)
+
+    foreach (var issue in issues)
+    {
+        Console.WriteLine(issue.Title);
+    };
+}
+else
+{
+    Console.WriteLine("No result found. Check your connection to the Database");
+}
+
+Console.ReadLine();
+
