6211dd9b414548b72f52300a0cbce1dbd8f1632d
Ilia Nikolaenko <niil02@sms-group.com>
2022-09-08 09:37:23 +0200

    Added action for PUT reqs to the Issue Controller"

--- TrackingAPI/Tracking-api/Controllers/IssueController.cs
+++ TrackingAPI/Tracking-api/Controllers/IssueController.cs
@@ -26,7 +26,7 @@ namespace Tracking_api.Controllers
             return await _context.Issues.ToListAsync();
         }
 
-        [HttpGet("{id}")]  // attr specifies that the action method is a handler for HTTP GET by id requests using the id placeholder
+        [HttpGet("{id}")]  // attr specifies that the action method is a handler for HTTP GET by id requests using the id placeholder in the url
         [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]  // the attr enhanses documentation of the action specifing which status code is returned
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetById(int id)
@@ -41,16 +41,35 @@ namespace Tracking_api.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<IActionResult> Create(Issue issue)
-        /* an async handler action for POST requests to the Issues table
+        /* an async handler action for POST requests to the Issues table to create a new instance in the table
          * Returns a CreatedAtAction obj upon successful updates of the DB table */
         {
             await _context.Issues.AddAsync(issue);  // adds a new instance to the Issues table of the DB
             await _context.SaveChangesAsync();  // propagates changes to be saved in the DB
 
-            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);  // CreatedAtAction returns a response code and the location. Params:
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);  // CreatedAtAction returns a response code and the location of the new instance (url: api/issue/NEWID).
+                                                                                    // Params:
                                                                                     // nameof: name of the action that returns a single instance from the DB
                                                                                     // anonymous obj for the id of the single instance
                                                                                     // the single instance itself
+        }
+
+        [HttpPut("{id}")]  // attr specifies that id placeholder is in the url
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        /* an async handler action for PUT requests to the Issues table to update an existing instance
+         * params:
+         *  id - cound to the url of the api
+         *  issue - bound to the body of the HTTP request
+         * Returns NoContent obj with the responce code 204 in case of successful update  */
+        {
+            if (id != issue.Id) return BadRequest();  // catches mismatch between ids in the url and in the body of the req
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();  // returns a 204 status code response with no data in the body
         }
 
     }
